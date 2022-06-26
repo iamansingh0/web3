6ce890f817f37eb5fe02b7e90dde77decf4ef2ad
iamansingh0 <amankumar7355197337@gmail.com>
2022-06-26 20:56:52 +0530

    deploying

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -1,5 +1,10 @@
+const ethers = require("ethers");
+
 async function main() {
-    
+    // http://0.0.0.0.7545  ganache rpc
+    //40d9224f8e1aaac84a6a9d092d834aa65dacab721e3ece9ee8f7792eff41a00d private key
+    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");
+    const wallet = new ethers.Wallet("40d9224f8e1aaac84a6a9d092d834aa65dacab721e3ece9ee8f7792eff41a00d", provider);P
 }
 
 main()
