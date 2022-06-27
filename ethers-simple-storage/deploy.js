081aafb04c514ef76d1f29671743ccaa1ec05846
iamansingh0 <amankumar7355197337@gmail.com>
2022-06-27 21:01:54 +0530

    deploying

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -2,14 +2,19 @@ const ethers = require("ethers");
 const fs = require("fs");
 
 async function main() {
-    // http://0.0.0.0.7545  ganache rpc
-    //bd029ee8e1a69a8f64cf0ec081ab5335b442157b396a0589e2838bcefe54719e private key
-    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");
+    // HTTP://172.20.64.1:7545  ganache rpc
+    //057625264bbe0a94ce0584a19e930b1fe61509949b02dd1b4b53103eba82f02b private key
+    const provider = new ethers.providers.JsonRpcProvider("http://172.20.64.1:7545");
     // ethers.Wallet(private key, rpc server)
-    const wallet = new ethers.Wallet("bd029ee8e1a69a8f64cf0ec081ab5335b442157b396a0589e2838bcefe54719e", provider);
+    const wallet = new ethers.Wallet("057625264bbe0a94ce0584a19e930b1fe61509949b02dd1b4b53103eba82f02b", provider);
 
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
     const bin = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
+    const contractFactory = new ethers.ContractFactory(abi, bin, wallet);
+    console.log("Delploying.....");
+    const contract = await contractFactory.deploy();
+    // await -> stop here, wait for the contract to deploy
+    console.log(contract);
 }
 
 main()
