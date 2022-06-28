ed4ecd8e290303f1b676fae1b073e6ccdd2372fc
iamansingh0 <amankumar7355197337@gmail.com>
2022-06-29 02:37:42 +0530

    simple calculator using ethers.js

---
+++ fundMe/IntCal.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract IntCal {
+    int public result;
+    function addition(int operand1, int operand2) public {
+        result = operand1 + operand2;
+    }
+    function subtraction(int operand1, int operand2) public {
+        result = operand1 - operand2;
+    }
+    function multiplication(int operand1, int operand2) public {
+        result = operand1 * operand2;
+    }
+    function division(int operand1, int operand2) public {
+        require(operand2 > 0, "Invalid Input");
+        require(operand1 > operand2, "Input 2 should be greater otherwise the result will be zero!");
+        result = operand1 / operand2;
+    }
+    function seeResults() public view returns(int) {
+        return result;
+    }
+}
