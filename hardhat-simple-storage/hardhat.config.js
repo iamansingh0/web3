bf9b4fd6e06aa872714678192d089a37f034730a
iamansingh0 <amankumar7355197337@gmail.com>
2022-06-29 16:43:15 +0530

    updated

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,14 +1,14 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners()
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,14 +16,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
 module.exports = {
-  defaultNetwork: "hardhat",
-  netoworks: {
-    rinkeby: {
-      url: process.env.RINKEBY_RPC_URL,
-      accounts: [process.env.RINKEBY_PRIVATE_KEY],
-      chainId: 4,
+    defaultNetwork: "hardhat",
+    netoworks: {
+        rinkeby: {
+            url: RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+        },
     },
-  },
-  solidity: "0.8.8",
-};
+    solidity: "0.8.8",
+}
