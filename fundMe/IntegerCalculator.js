4d80e9a2b732d434da77962bc49a34417376e0bd
iamansingh0 <amankumar7355197337@gmail.com>
2022-06-29 15:01:41 +0530

    updated

--- fundMe/IntegerCalculator.js
+++ fundMe/IntegerCalculator.js
@@ -5,18 +5,19 @@ require('dotenv').config();
 async function main() {
     const provider = new ethers.providers.JsonRpcProvider(process.env.rpcURL);
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY , provider);
+
     const abi = fs.readFileSync("./IntCal_sol_IntCal.abi", "utf8");
     const bin = fs.readFileSync("./IntCal_sol_IntCal.bin", "utf8");
     const contractFactory = new ethers.ContractFactory(abi, bin, wallet);
     console.log("Delploying.....");
     const contract = await contractFactory.deploy();
     const transactionReceipt = await contract.deployTransaction.wait(1);
-    console.log(`Contract Address: ${contract.address}`);
+    // console.log(`Contract Address: ${contract.address}`);
     // console.log(contract);
-    const addResponse = await contract.addition("69", "34");
-    const addResponseReceipt = await addResponse.wait(1);
+    const subResponse = await contract.subtraction("69", "34");
+    const subResponseReceipt = await subResponse.wait(1);
     const result = await contract.seeResults();
-    console.log(`Addition of 69 and 34 is: ${result.toString()}`);
+    console.log(`Subtration of 69 and 34 is: ${result.toString()}`);
 }
 
 main()
