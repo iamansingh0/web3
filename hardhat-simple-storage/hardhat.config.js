acb3b625e610f5bf6751e97d63e43fe9a7de83ae
iamansingh0 <amankumar7355197337@gmail.com>
2022-06-29 16:24:42 +0530

    update

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
   netoworks: {
     rinkeby: {
       url: process.env.RINKEBY_RPC_URL,
-      account: [process.env.RINKEBY_PRIVATE_KEY],
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
       chainId: 4,
     },
   },
