62d834e2893572aee213b075ca6b4ed7a20eb23d
iamansingh0 <amankumar7355197337@gmail.com>
2022-07-01 03:00:33 +0530

    update

--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -8,23 +8,37 @@ async function main() {
     const simpleStorage = await contractFactory.deploy()
     await simpleStorage.deployed()
     console.log(`deployed contract to: ${simpleStorage.address}`)
+    if (network.config.chainId === 4 && process.env.apiKey) {
+        await simpleStorage.deployTransaction.wait(6)
+        console.log("Blocks confirmation completed!")
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+
+    // updating current value
+    const updatingValue = await simpleStorage.store(3)
+    await updatingValue.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
 }
 
 async function verify(contractAddress, args) {
-  // use hardhat etherscan plugin
-  console.log("Verifying contract..");
-  try {
-      await run("verify:verify", {
-          address: contractAddress,
-          constructorArguments: args,
-      })
-  } catch(e) {
-      if(e.message.toLowerCase().includes("already verified")) {
-          console.log("Already verified")
-      } else {
-          console.log(e)
-      }
-  }
+    // use hardhat etherscan plugin
+    console.log("Verifying contract..")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 main()
